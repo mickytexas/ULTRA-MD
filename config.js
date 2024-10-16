b1913ded5cb83b5ba845b430f15acc9c34691e79
mickytexas <137762382+mickytexas@users.noreply.github.com>
2024-10-16 12:52:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "923444844060"
+const ownervb = process.env.OWNERS || "2348103561817"
 
 
 const ownerlist = ownervb.split(';');
@@ -22,7 +22,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 }
 
 //global.pairingNumber = "92xxx_number" //put your bot number here
-global.botNumber = '923051391007' //put your bot number here
+global.botNumber = '2348103561817' //put your bot number here
 
 global.mods = ['923444844060', '923051391007']
 global.prems = ['923444844060', '923051391007']
